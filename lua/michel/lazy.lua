918b50ba8e916dced486519c55037c68abf521b3
CodeLikeObama <michel.lentzler@gmail.com>
2024-10-13 17:47:11 +0200

    adds mason.nvim

--- lua/michel/lazy.lua
+++ lua/michel/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("michel.plugins", {
+require("lazy").setup({ { import = "michel.plugins" }, { import = "michel.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
--- lua/michel/plugins/autopairs.lua
+++ lua/michel/plugins/autopairs.lua
@@ -22,7 +22,7 @@ return {
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
     -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
+    local cmp = require("cmp") 
 
     -- make autopairs and completion work together
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
