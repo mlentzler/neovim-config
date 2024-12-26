a34eab2cc6446be0290c224ccbcb88c2ef896f7a
CodeLikeObama <michel.lentzler@gmail.com>
2024-12-26 18:32:43 +0100

    updates and adds markdownpreview

--- lua/michel/plugins/markdown-preview.lua
+++ lua/michel/plugins/markdown-preview.lua
@@ -1,22 +1,6 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
-  config = function()
-    vim.keymap.set(
-      'n',
-      '<leader>md',
-      '<cmd>MarkdownPreview<CR>',
-      { desc = 'Preview markdown in browser', buffer = true }
-    )
-    vim.keymap.set(
-      'n',
-      '<leader>mds',
-      '<cmd>MarkdownPreviewStop<CR>',
-      { desc = 'Stop markdown preview', buffer = true }
-    )
-  end,
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 }
