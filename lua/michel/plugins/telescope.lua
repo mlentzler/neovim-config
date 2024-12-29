82a30c70e58773834c23594db107985e4d15b5f4
CodeLikeObama <michel.lentzler@gmail.com>
2024-12-29 10:25:44 +0100

    Adds todo-comments-nvim

--- lua/michel/plugins/telescope.lua
+++ lua/michel/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -35,5 +36,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>fn", "<cmd>Telescope noice<cr>", { desc = "Search noice messages" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
   end,
 }
