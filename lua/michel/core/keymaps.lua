cdcd4db6d4d85a2e9998bf30220aa4ac9ebe19be
mlentzler <michel.lentzler@gmail.com>
2025-08-13 11:43:57 +0200

    Changes tab open and tap delete to new shortcuts

--- lua/michel/core/keymaps.lua
+++ lua/michel/core/keymaps.lua
@@ -2,11 +2,10 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
-     
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" }) -- clear search highlights
 
-keymap.set('n', 'U', '<cmd>redo<CR>', { desc = 'Redo last change' }) -- Redo changes like an undo with U
+keymap.set("n", "U", "<cmd>redo<CR>", { desc = "Redo last change" }) -- Redo changes like an undo with U
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
@@ -15,8 +14,8 @@ keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- 
 keymap.set("n", "<leader>sd", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 -- tabs
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>td", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tw", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
@@ -24,11 +23,10 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 -- Map leader + 1-9 to switch to tab 1-9
 
 for i = 1, 9 do
-  vim.keymap.set(
-    'n',
-    '<leader>' .. i,
-    ':tabn ' .. i .. '<CR>',
-    { silent = true, desc = 'which_key_ignore' } -- which_key_irgonre makes them not show up in which-key.nvim
-  )
+	vim.keymap.set(
+		"n",
+		"<leader>" .. i,
+		":tabn " .. i .. "<CR>",
+		{ silent = true, desc = "which_key_ignore" } -- which_key_irgonre makes them not show up in which-key.nvim
+	)
 end
-
